0f5d0b6ae45285e4dd281a1f95c371c2b048434d
Eddie A Tejeda <eddie@codeforamerica.org>
2013-07-01 20:20:41 -0700

    use ENV for secret key and create new sample key as well

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,12 +9,6 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
-	raise 'The SECRET_TOKEN environment variable is not set.\n
-	To generate it, run "rake secret", then set it on the production server.\n
-  If you\'re using Heroku, you do this with "heroku config:set SECRET_TOKEN=the_token_you_generated"'
-end
-
 Openblight::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '592921b99944819ea2758b5df143e4ce8ac14d3bcb77c52a2f41d41f24ed1f7eed209f44e3ae18116e9424aaf430a1ba7ebb2bb644bb9964545a19ee71a52f4e'
 
 
